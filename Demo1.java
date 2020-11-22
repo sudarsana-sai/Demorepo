34ad33c6547f4df8e1cb4c7b0065f94a6fdc6218
saidevops <saidevops@ip-172-31-17-195.us-east-2.compute.internal>
2020-11-22 17:31:32 +0000

    class file addition

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,5 @@
 class Demo1{
-	public static void main(){
-		System.out.println("This is Demo for Git")
+	public static void main(String args[]){
+		System.out.println("This is Demo for Git");
 	}
 }
