84590cc5c55394ee347433917ca656802e3cc26d
saidevops <saidevops@ip-172-31-17-195.us-east-2.compute.internal>
2020-11-28 05:09:22 +0000

    two files added

--- Demo1.java
+++ Demo1.java
@@ -2,5 +2,6 @@ class Demo1{
 	public static void main(String args[]){
 		System.out.println("This is Demo for Git");
 		System.out.println("Second line added");
+		System.out.println("Third line addition");
 	}
 }
--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,5 @@
-class Demo1{
+class Demo2{
 	public static void main(String args[]){
-		System.out.println("This is Demo for Git");
+		System.out.println("This is Demo2 for Git");
 	}
 }
