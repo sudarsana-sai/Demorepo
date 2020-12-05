d59bde53b950c8046ec3da1c7474894215e40bf7
saidevops <saidevops@ip-172-31-17-195.us-east-2.compute.internal>
2020-12-05 18:35:41 +0000

    change in feature1

--- Demo3.java
+++ Demo3.java
@@ -2,6 +2,6 @@ class Demo3{
 	public static void main(String args[]){
 		System.out.println("This is Demo3 for Git");
 		System.out.println("added in remote");
-		System.out.println("change in feature1");
+		System.out.println("change in feature1 again");
 	}
 }
