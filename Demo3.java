1ab866b2f8935869ea8c0bd02b88746c9c492a32
sudarsana meruva <sudarsan.sai@gmail.com>
2021-02-17 15:37:23 -0700

    revert test

--- Demo3.java
+++ Demo3.java
@@ -2,7 +2,6 @@ class Demo3{
 	public static void main(String args[]){
 		System.out.println("This is Demo3 for Git");
 		System.out.println("added in remote");
-		System.out.println("merged version");
 	}
 //adding a comment
 
